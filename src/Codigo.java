529ecb11dc0c1a86d48f652ae0b102541debaabf
Francisco <takincillo@hotmail.com>
2024-09-27 17:21:17 -0600

    se arregla el error de logica y diferentes errores de sintaxis

--- src/Codigo.java
+++ src/Codigo.java
@@ -1,39 +1,56 @@
 import java.util.Scanner;
 //se importa dependencia de scanner
 public class Codigo {
-	//se completa la entrada de scanner
-    Scanner s = new Scanner(System.in);
-    
-    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-    String j1 = s.nextLine();
-    
-    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-    Scanner s2 = new Scanner();
-    String j2 = s.nextLine();
-    
-    if (j1 == j2)) {
-      System.out.println("Empate");
-    } else {
-      int g = 2;
-      switch(j1) {
-        case "piedra":
-          if (j2 == "tijeras") {
-            g = 1;
-          }
+	//se agrega metodo main para poder correr funciones
+	public static void main(String[] args) {
+		
+		//se completa la entrada de scanner
+	    Scanner s = new Scanner(System.in);
+	    
+	    System.out.println("Turno del jugador 1 introduzca piedra, papel o tijeras: ");
+	    String j1 = s.nextLine();
+	    //se corrige texto para mencionar al jugador 2
+	    System.out.print("Turno del jugador 2 (introduzca piedra, papel o tijeras): ");
+	    Scanner s2 = new Scanner(System.in);
+	   //se elimina instancia extra del scanner
+	    String j2 = s2.nextLine();
+	    //se borra parentesis sobrante
+	    //se agregan breaks a cada case
+	    //se cambia el == por equals para una comparacion correcta
+	    if (j1.equals(j2)) {
+	      System.out.println("Empate");
+	    } else {
+	      int g = 2;
+	      switch(j1) {
+	        case "piedra":
+	          if (j2.equals("tijeras")) {
+	            g = 1;
+	            break;
+	          }
 
-        case "papel":
-          if (j2 == "piedra") {
-            g = 1;
-          
-        case "tijera":
-          if (j2.equals("papel")) {
-            g = 1;
-          }
-          break;
-        default:
-      }
-      System.out.println("Gana el jugador " + g);
-    }
-  
-  
-}
+	        case "papel":
+	          if (j2.equals("piedra")) {
+	             g = 1;
+	             break;
+	          }
+	          //se agrega bracket faltante
+	        case "tijeras":
+	        	//se cambia el . queals por una comparacion con ==
+	          if (j2.equals("papel")) {
+	            g = 1;
+	            break;
+	          }
+	          break;
+	        default:
+	      }
+	      System.out.println("Gana el jugador " + g);
+
+			
+		
+	}//main
+	
+	
+	}//main
+	
+}//class
+	
